9ac019f7a0a3c80b6d59d6b5d8e6144855938c25
Khanke Suraj <surajkhanke786@gmail.com>
2025-01-30 11:47:39 +0530

    imdb clone

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-yellow-400'>hi this is me suraj</h1>
+      <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+    </>
+  )
+}
+
+export default App
